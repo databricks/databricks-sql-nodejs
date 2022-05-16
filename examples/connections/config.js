08b225518f7ad22e35fe826fc0816f09b4cb573b
Levko Kravets <levko.ne@gmail.com>
2022-05-16 16:39:28 +0300

    Remove irrelevant examples and unused dependencies

--- examples/connections/config.js
+++ examples/connections/config.js
@@ -1,9 +1,3 @@
-const fqdn = require('get-fqdn');
-const path = require('path');
+module.exports = () => Promise.resolve({
 
-module.exports = () => fqdn().then(hostname => ({
-    hostname,
-    ca: path.resolve(__dirname, '../../.docker/ssl/ca.pem'),
-    cert: path.resolve(__dirname, '../../.docker/ssl/cert.pem'),
-    key: path.resolve(__dirname, '../../.docker/ssl/key.key'),
-}));
+});
