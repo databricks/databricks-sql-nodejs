a5bcb08a07c5673807f871b0ffab13f87eb7b9b0
Yunbo Deng <yunbo.deng@databricks.com>
2022-06-24 18:03:44 -0700

    Another minor fixes to the config js file

--- tests/e2e/utils/config.js
+++ tests/e2e/utils/config.js
@@ -8,8 +8,9 @@ try {
 module.exports = {
     // Where to log: CONSOLE, FILE, QUIET
     logger: 'CONSOLE',
+    // Replace the following values with the actual connection details.
     host: '***.databricks.com',
-    path: '/sql/1.0/endpoints/5***',
+    path: '/sql/1.0/endpoints/***',
     token: 'dapi***',
     // Make sure your client and credential actually have CREATE permission in the database.
     database: ['catalog', 'database'],
