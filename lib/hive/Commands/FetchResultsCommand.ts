fc8cc1bcf1cf36611d4ad50ed25cf849918e7d93
Levko Kravets <levko.ne@gmail.com>
2022-07-27 11:06:18 +0300

    Restore some important comments

--- lib/hive/Commands/FetchResultsCommand.ts
+++ lib/hive/Commands/FetchResultsCommand.ts
@@ -1,6 +1,9 @@
 import BaseCommand from './BaseCommand';
 import { TFetchResultsReq, TFetchResultsResp } from '../../../thrift/TCLIService_types';
 
+/**
+ * TFetchResultsReq.fetchType - 0 represents Query output. 1 represents Log
+ */
 export default class FetchResultsCommand extends BaseCommand {
   execute(data: TFetchResultsReq): Promise<TFetchResultsResp> {
     const request = new TFetchResultsReq(data);
--- lib/hive/Commands/OpenSessionCommand.ts
+++ lib/hive/Commands/OpenSessionCommand.ts
@@ -1,6 +1,15 @@
 import BaseCommand from './BaseCommand';
 import { TOpenSessionReq, TOpenSessionResp } from '../../../thrift/TCLIService_types';
 
+/**
+ * For auth mechanism GSSAPI the host and service should be provided when session is opened.
+ *
+ * TOpenSessionReq.configuration: {
+ *   krb_host?: string;
+ *   krb_service?: string;
+ *   [key: string]: any;
+ * }
+ */
 export default class OpenSessionCommand extends BaseCommand {
   execute(openSessionRequest: TOpenSessionReq): Promise<TOpenSessionResp> {
     const request = new TOpenSessionReq(openSessionRequest);
