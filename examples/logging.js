0e26cf5781272e47485b33be21a4aff950417bec
nithinkdb <nithin.krishnamurthi@databricks.com>
2022-10-24 12:14:26 -0700

    Changed comment
    Signed-off-by: nithinkdb <nithin.krishnamurthi@databricks.com>

--- examples/logging.js
+++ examples/logging.js
@@ -1,16 +1,17 @@
+const { LOADIPHLPAPI } = require('dns');
 const { DBSQLClient, DBSQLLogger, LogLevel } = require('../');
 
-// This logger will emit logs to console only.
+// This logger will emit logs to console and log.txt
 //
 const logger = new DBSQLLogger('log.txt', LogLevel.info);
 
-const client = new DBSQLClient(logger);
+const client = new DBSQLClient({logger: logger});
 
 client
   .connect({
-    host: '********.databricks.com',
-    path: '/sql/1.0/endpoints/****************',
-    token: 'dapi********************************',
+    host: 'eng-academy.staging.cloud.databricks.com',
+    path: '/sql/1.0/endpoints/410886e8ec4b0796',
+    token: 'dapi5aef9f11ea0f3003440555526226ef7f',
   })
   .then(async (client) => {
     const session = await client.openSession();
@@ -23,7 +24,7 @@ client
 
     // Set logger to different level.
     //
-    logger.setLevel(LOGLEVELS.debug);
+    logger.setLevel(LogLevel.debug);
 
     queryOperation = await session.executeStatement('SELECT "Hello, World!"', { runAsync: true });
     result = await queryOperation.fetchAll();
