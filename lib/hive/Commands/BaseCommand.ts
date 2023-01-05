13b1108e07cd5639a49f090e5ec8576f54194da1
nithinkdb <nithin.krishnamurthi@databricks.com>
2023-01-04 16:14:33 -0800

    Added retry helper
    Signed-off-by: nithinkdb <nithin.krishnamurthi@databricks.com>

--- lib/hive/Commands/BaseCommand.ts
+++ lib/hive/Commands/BaseCommand.ts
@@ -1,6 +1,8 @@
+import { type } from 'os';
 import TCLIService from '../../../thrift/TCLIService';
 import HiveDriverError from '../../errors/HiveDriverError';
 import errorHandler from '../../utils/errorHandler';
+import { TApplicationException, TProtocolException } from '../..';
 
 export default abstract class BaseCommand {
   protected client: TCLIService.Client;
@@ -10,27 +12,6 @@ export default abstract class BaseCommand {
   }
 
   executeCommand<Response>(request: object, command: Function | void): Promise<Response> {
-    // return errorHandler<Response>(this.client, request, command);
-    return new Promise((resolve, reject) => {
-      if (typeof command !== 'function') {
-        reject(new HiveDriverError('Hive driver: the operation does not exist, try to choose another Thrift file.'));
-        return;
-      }
-
-      try {
-        console.log("Here1");
-        command.call(this.client, request, (err: Error, response: Response) => {
-          console.log("Here2");
-          if (err) {
-            reject(err);
-          } else {
-            resolve(response);
-          }
-        });
-      } catch (error) {
-        console.log("Here3");
-        reject(error);
-      }
-    });
+    return errorHandler<Response>(this.client, request, command, 0, Date.now());
   }
 }
--- lib/utils/errorHandler.ts
+++ lib/utils/errorHandler.ts
@@ -2,7 +2,7 @@ import TCLIService from "../../thrift/TCLIService"
 import HiveDriverError from "../errors/HiveDriverError";
 import { Thrift } from "thrift";
 
-export default function errorHandler<Response>(client: TCLIService.Client, request: object, command: Function | void): Promise<Response>{
+export default function errorHandler<Response>(client: TCLIService.Client, request: object, command: Function | void, retries: number, startTime: number): Promise<Response>{
     return new Promise((resolve, reject) => {
         if (typeof command !== 'function') {
             reject(new HiveDriverError('Hive driver: the operation does not exist, try to choose another Thrift file.'));
@@ -10,25 +10,35 @@ export default function errorHandler<Response>(client: TCLIService.Client, reque
         }
         try {
             command.call(client, request, (err: Error, response: Response) => {
-            console.log("Trying");
             if(response) {
                 resolve(response);
                 return;
             }
             if (err instanceof Thrift.TApplicationException) {
-                switch(err.code) {
-                    case 404:
-                        reject(new HiveDriverError('Hive driver: 404 when connecting to resource. Check the host provided.'));
-                        return;
-                    case 403:
-                        reject(new HiveDriverError('Hive driver: 403 when connecting to resource. Check the token used to authenticate.'));
-                        return;
-                    case 401:
-                        reject(new HiveDriverError('Hive driver: 401 when connecting to resource. Check the path provided.'));
-                        return;
-                    default:
-                        reject(err);
-                        return;
+                if('statusCode' in err) {
+                    switch(err.statusCode) {
+                        case 429:
+                        case 503:
+                            if(Date.now() - startTime > 15000) {
+                                reject(err);
+                                return;
+                            }
+                            else {
+                                return errorHandler(client,request,command,retries+1,startTime);
+                            }
+                        case 404:
+                            reject(new HiveDriverError('Hive driver: 404 when connecting to resource. Check the host provided.'));
+                            return;
+                        case 403:
+                            reject(new HiveDriverError('Hive driver: 403 when connecting to resource. Check the token used to authenticate.'));
+                            return;
+                        case 401:
+                            reject(new HiveDriverError('Hive driver: 401 when connecting to resource. Check the path provided.'));
+                            return;
+                        default:
+                            reject(err);
+                            return;
+                    }
                 }
             }
             else {
