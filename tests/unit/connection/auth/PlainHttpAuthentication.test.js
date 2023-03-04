63967da1afd8f8982fe3b7526e8b9ae7c54e06b2
divyavanmahajan <divyavanmahajan@users.noreply.github.com>
2023-03-04 09:43:42 +0100

    Add unit tests for AAD token support
    Signed-off-by: divyavanmahajan <divyavanmahajan@users.noreply.github.com>

--- tests/unit/connection/auth/PlainHttpAuthentication.test.js
+++ tests/unit/connection/auth/PlainHttpAuthentication.test.js
@@ -1,4 +1,5 @@
 const { expect } = require('chai');
+const { useAADToken } = require('../../../e2e/utils/config.local');
 const PlainHttpAuthentication = require('../../../../dist/connection/auth/PlainHttpAuthentication').default;
 
 describe('PlainHttpAuthentication', () => {
@@ -17,6 +18,7 @@ describe('PlainHttpAuthentication', () => {
 
     expect(auth.username).to.be.eq('user');
     expect(auth.password).to.be.eq('pass');
+    expect(auth.useAADToken).to.be.eq(false);
   });
 
   it('empty password must be set', () => {
@@ -27,14 +29,32 @@ describe('PlainHttpAuthentication', () => {
 
     expect(auth.username).to.be.eq('user');
     expect(auth.password).to.be.eq('');
+    expect(auth.useAADToken).to.be.eq(false);
   });
 
-  it('auth token must be set to header', () => {
+  it('auth (PAT) token must be set to header', () => {
     const auth = new PlainHttpAuthentication();
     const transportMock = {
       setOptions(name, value) {
         expect(name).to.be.eq('headers');
         expect(value.Authorization).to.be.eq('Basic YW5vbnltb3VzOmFub255bW91cw==');
+      },
+    };
+    return auth.authenticate(transportMock).then((transport) => {
+      expect(transport).to.be.eq(transportMock);
+    });
+  });
+
+  it('auth (Azure) token must be set to header', () => {
+    const auth = new PlainHttpAuthentication({
+      username: 'user',
+      password: 'azureadtoken',
+      useAADToken: true,
+    });
+    const transportMock = {
+      setOptions(name, value) {
+        expect(name).to.be.eq('headers');
+        expect(value.Authorization).to.be.eq('Bearer azureadtoken');
       },
     };
     return auth.authenticate(transportMock).then((transport) => {
