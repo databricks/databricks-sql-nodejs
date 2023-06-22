3fe269c0eb34125dd4f7477ec641db1b36bd244a
Levko Kravets <levko.ne@gmail.com>
2023-06-22 11:39:01 +0300

    Fix: move comment to appropriate place
    Signed-off-by: Levko Kravets <levko.ne@gmail.com>

--- lib/connection/auth/DatabricksOAuth/OAuthToken.ts
+++ lib/connection/auth/DatabricksOAuth/OAuthToken.ts
@@ -19,6 +19,10 @@ export default class OAuthToken {
   }
 
   get expirationTime(): number {
+    // This token has already been verified, and we are just parsing it.
+    // If it has been tampered with, it will be rejected on the server side.
+    // This avoids having to fetch the public key from the issuer and perform
+    // an unnecessary signature verification.
     if (this._expirationTime === undefined) {
       const accessTokenPayload = Buffer.from(this._accessToken.split('.')[1], 'base64').toString('utf8');
       const decoded = JSON.parse(accessTokenPayload);
@@ -28,10 +32,6 @@ export default class OAuthToken {
   }
 
   get hasExpired(): boolean {
-    // This token has already been verified, and we are just parsing it.
-    // If it has been tampered with, it will be rejected on the server side.
-    // This avoids having to fetch the public key from the issuer and perform
-    // an unnecessary signature verification.
     const now = Math.floor(Date.now() / 1000); // convert it to seconds
     return this.expirationTime <= now;
   }
