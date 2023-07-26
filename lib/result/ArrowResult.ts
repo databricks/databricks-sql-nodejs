4bdf1b7b7d1d0b3020a3cb3e89d7cf50d6a13d0b
Levko Kravets <levko.ne@gmail.com>
2023-07-26 12:38:43 +0300

    Preserve null values when parsing arrow data
    Signed-off-by: Levko Kravets <levko.ne@gmail.com>

--- lib/result/ArrowResult.ts
+++ lib/result/ArrowResult.ts
@@ -68,6 +68,10 @@ export default class ArrowResult implements IOperationResult {
   }
 
   private convertArrowTypes(value: any, valueType: DataType | undefined, fields: Array<ArrowSchemaField> = []): any {
+    if (value === null) {
+      return value;
+    }
+
     const fieldsMap: Record<string, ArrowSchemaField> = {};
     for (const field of fields) {
       fieldsMap[field.name] = field;
@@ -101,7 +105,7 @@ export default class ArrowResult implements IOperationResult {
     }
 
     if (DataType.isTimestamp(valueType)) {
-      return value ? new Date(value) : null;
+      return new Date(value);
     }
 
     // Convert big number values to BigInt
