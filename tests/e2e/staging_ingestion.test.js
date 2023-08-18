25bf802ec96250b85ebfeaa693f32551e25d4e55
Levko Kravets <levko.ne@gmail.com>
2023-08-18 16:18:42 +0300

    Remove duplicate test
    Signed-off-by: Levko Kravets <levko.ne@gmail.com>

--- tests/e2e/staging_ingestion.test.js
+++ tests/e2e/staging_ingestion.test.js
@@ -1,9 +1,7 @@
 const { expect } = require('chai');
 const config = require('./utils/config');
-const logger = require('./utils/logger')(config.logger);
 const { DBSQLClient } = require('../..');
 const fs = require('fs');
-const globalConfig = require('../../dist/globalConfig').default;
 
 describe('Staging Test', () => {
   it('put staging data and receive it', async () => {
@@ -17,32 +15,6 @@ describe('Staging Test', () => {
     fs.writeFileSync(tempPath, 'Hello World!');
 
     const session = await client.openSession({
-      initialCatalog: config.database[0],
-      initialSchema: config.database[1],
-    });
-    await session.executeStatement(
-      `PUT '${tempPath}' INTO '/Volumes/${config.database[0]}/${config.database[1]}/e2etests/file1.csv' OVERWRITE`,
-      { stagingAllowedLocalPath: ['tests/e2e/staging'] },
-    );
-    await session.executeStatement(
-      `GET '/Volumes/${config.database[0]}/${config.database[1]}/e2etests/file1.csv' TO 'tests/e2e/staging/file'`,
-      { stagingAllowedLocalPath: ['tests/e2e/staging'] },
-    );
-    let result = fs.readFileSync('tests/e2e/staging/file');
-    expect(result.toString() === 'Hello World!').to.be.true;
-  });
-
-  it('put staging data and receive it', async () => {
-    const client = new DBSQLClient();
-    await client.connect({
-      host: config.host,
-      path: config.path,
-      token: config.token,
-    });
-    let tempPath = 'tests/e2e/staging/data';
-    fs.writeFileSync(tempPath, (data = 'Hello World!'));
-
-    let session = await client.openSession({
       initialCatalog: config.database[0],
       initialSchema: config.database[1],
     });
