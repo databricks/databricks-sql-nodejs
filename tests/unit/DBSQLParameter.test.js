da6bb626cf081935ae8c1a1afbc9b1924c460391
Nithin Krishnamurthi <110488533+nithinkdb@users.noreply.github.com>
2023-09-13 03:30:25 -0700

    [PECO-1052] Added enum type class to DBSQLParameter class (#175)
    * Added enum
    
    Signed-off-by: nithinkdb <nithin.krishnamurthi@databricks.com>
    
    * Done
    
    Signed-off-by: nithinkdb <nithin.krishnamurthi@databricks.com>
    
    ---------
    
    Signed-off-by: nithinkdb <nithin.krishnamurthi@databricks.com>

--- tests/unit/DBSQLParameter.test.js
+++ tests/unit/DBSQLParameter.test.js
@@ -2,28 +2,67 @@ const { expect } = require('chai');
 
 const Int64 = require('node-int64');
 const { TSparkParameterValue, TSparkParameter } = require('../../thrift/TCLIService_types');
-const { default: DBSQLParameter } = require('../../dist/DBSQLParameter');
+const { DBSQLParameter, DBSQLParameterType } = require('../../dist/DBSQLParameter');
 
 describe('DBSQLParameter', () => {
   it('should infer types correctly', () => {
     const cases = [
-      [false, new TSparkParameter({ type: 'BOOLEAN', value: new TSparkParameterValue({ stringValue: 'FALSE' }) })],
-      [true, new TSparkParameter({ type: 'BOOLEAN', value: new TSparkParameterValue({ stringValue: 'TRUE' }) })],
-      [123, new TSparkParameter({ type: 'INTEGER', value: new TSparkParameterValue({ stringValue: '123' }) })],
-      [3.14, new TSparkParameter({ type: 'DOUBLE', value: new TSparkParameterValue({ stringValue: '3.14' }) })],
-      [BigInt(1234), new TSparkParameter({ type: 'BIGINT', value: new TSparkParameterValue({ stringValue: '1234' }) })],
+      [
+        false,
+        new TSparkParameter({
+          type: DBSQLParameterType.BOOLEAN,
+          value: new TSparkParameterValue({ stringValue: 'FALSE' }),
+        }),
+      ],
+      [
+        true,
+        new TSparkParameter({
+          type: DBSQLParameterType.BOOLEAN,
+          value: new TSparkParameterValue({ stringValue: 'TRUE' }),
+        }),
+      ],
+      [
+        123,
+        new TSparkParameter({
+          type: DBSQLParameterType.INTEGER,
+          value: new TSparkParameterValue({ stringValue: '123' }),
+        }),
+      ],
+      [
+        3.14,
+        new TSparkParameter({
+          type: DBSQLParameterType.DOUBLE,
+          value: new TSparkParameterValue({ stringValue: '3.14' }),
+        }),
+      ],
+      [
+        BigInt(1234),
+        new TSparkParameter({
+          type: DBSQLParameterType.BIGINT,
+          value: new TSparkParameterValue({ stringValue: '1234' }),
+        }),
+      ],
       [
         new Int64(1234),
-        new TSparkParameter({ type: 'BIGINT', value: new TSparkParameterValue({ stringValue: '1234' }) }),
+        new TSparkParameter({
+          type: DBSQLParameterType.BIGINT,
+          value: new TSparkParameterValue({ stringValue: '1234' }),
+        }),
       ],
       [
         new Date('2023-09-06T03:14:27.843Z'),
         new TSparkParameter({
-          type: 'TIMESTAMP',
+          type: DBSQLParameterType.TIMESTAMP,
           value: new TSparkParameterValue({ stringValue: '2023-09-06T03:14:27.843Z' }),
         }),
       ],
-      ['Hello', new TSparkParameter({ type: 'STRING', value: new TSparkParameterValue({ stringValue: 'Hello' }) })],
+      [
+        'Hello',
+        new TSparkParameter({
+          type: DBSQLParameterType.STRING,
+          value: new TSparkParameterValue({ stringValue: 'Hello' }),
+        }),
+      ],
     ];
 
     for (const [value, expectedParam] of cases) {
