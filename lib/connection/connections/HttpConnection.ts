c968016a2e301a02e3dee687965eb6f67d27b8bd
Levko Kravets <levko.ne@gmail.com>
2023-09-25 13:24:28 +0300

    Implement custom HttpConnection for Thrift library
    Signed-off-by: Levko Kravets <levko.ne@gmail.com>

--- lib/connection/connections/HttpConnection.ts
+++ lib/connection/connections/HttpConnection.ts
@@ -1,50 +1,52 @@
 import thrift from 'thrift';
 import https from 'https';
-import http, { IncomingMessage } from 'http';
+import http from 'http';
 
 import IThriftConnection from '../contracts/IThriftConnection';
 import IConnectionProvider from '../contracts/IConnectionProvider';
-import IConnectionOptions, { Options } from '../contracts/IConnectionOptions';
+import IConnectionOptions from '../contracts/IConnectionOptions';
 import globalConfig from '../../globalConfig';
 
-type NodeOptions = {
-  ca?: Buffer | string;
-  cert?: Buffer | string;
-  key?: Buffer | string;
-  rejectUnauthorized?: boolean;
-};
+import ThriftHttpConnection from './ThriftHttpConnection';
 
 export default class HttpConnection implements IConnectionProvider, IThriftConnection {
-  private thrift = thrift;
-
   private connection: any;
 
-  async connect(options: IConnectionOptions): Promise<IThriftConnection> {
-    const agentOptions: http.AgentOptions = {
+  private async getAgent(options: IConnectionOptions): Promise<http.Agent> {
+    const httpAgentOptions: http.AgentOptions = {
       keepAlive: true,
       maxSockets: 5,
       keepAliveMsecs: 10000,
-      timeout: options.options?.socketTimeout ?? globalConfig.socketTimeout,
+      timeout: options.socketTimeout ?? globalConfig.socketTimeout,
     };
 
-    const agent = options.options?.https
-      ? new https.Agent({ ...agentOptions, minVersion: 'TLSv1.2' })
-      : new http.Agent(agentOptions);
+    const httpsAgentOptions: https.AgentOptions = {
+      ...httpAgentOptions,
+      minVersion: 'TLSv1.2',
+      rejectUnauthorized: false,
+      ca: options.ca,
+      cert: options.cert,
+      key: options.key,
+    };
 
-    const thriftOptions = {
-      transport: thrift.TBufferedTransport,
-      protocol: thrift.TBinaryProtocol,
-      ...options.options,
-      nodeOptions: {
-        agent,
-        ...this.getNodeOptions(options.options || {}),
-        ...(options.options?.nodeOptions || {}),
-        timeout: options.options?.socketTimeout ?? globalConfig.socketTimeout,
+    return options.https ? new https.Agent(httpsAgentOptions) : new http.Agent(httpAgentOptions);
+  }
+
+  async connect(options: IConnectionOptions): Promise<IThriftConnection> {
+    const agent = await this.getAgent(options);
+
+    this.connection = new ThriftHttpConnection(
+      {
+        url: `${options.https ? 'https' : 'http'}://${options.host}:${options.port}${options.path ?? '/'}`,
+        transport: thrift.TBufferedTransport,
+        protocol: thrift.TBinaryProtocol,
       },
-    };
-
-    this.connection = this.thrift.createHttpConnection(options.host, options.port, thriftOptions);
-    this.addCookieHandler();
+      {
+        agent,
+        timeout: options.socketTimeout ?? globalConfig.socketTimeout,
+        headers: options.headers,
+      },
+    );
 
     return this;
   }
@@ -54,47 +56,6 @@ export default class HttpConnection implements IConnectionProvider, IThriftConne
   }
 
   isConnected(): boolean {
-    if (this.connection) {
-      return true;
-    }
-    return false;
-  }
-
-  private getNodeOptions(options: Options): object {
-    const { ca, cert, key, https: useHttps } = options;
-    const nodeOptions: NodeOptions = {};
-
-    if (ca) {
-      nodeOptions.ca = ca;
-    }
-    if (cert) {
-      nodeOptions.cert = cert;
-    }
-    if (key) {
-      nodeOptions.key = key;
-    }
-
-    if (useHttps) {
-      nodeOptions.rejectUnauthorized = false;
-    }
-
-    return nodeOptions;
-  }
-
-  private addCookieHandler() {
-    const { responseCallback } = this.connection;
-
-    this.connection.responseCallback = (response: IncomingMessage, ...rest: Array<unknown>) => {
-      if (Array.isArray(response.headers['set-cookie'])) {
-        const cookie = [this.connection.nodeOptions.headers.cookie];
-
-        this.connection.nodeOptions.headers.cookie = cookie
-          .concat(response.headers['set-cookie'])
-          .filter(Boolean)
-          .join(';');
-      }
-
-      responseCallback.call(this.connection, response, ...rest);
-    };
+    return !!this.connection;
   }
 }
---
+++ lib/connection/connections/ThriftHttpConnection.ts
@@ -0,0 +1,161 @@
+/**
+  This file is created using node_modules/thrift/lib/nodejs/lib/thrift/http_connection.js as an example
+
+  The code relies on thrift internals, so be careful when upgrading `thrift` library
+*/
+
+import { EventEmitter } from 'events';
+import { TBinaryProtocol, TBufferedTransport, Thrift, TProtocol, TProtocolConstructor, TTransport } from 'thrift';
+import fetch, { RequestInit, Response, FetchError } from 'node-fetch';
+// @ts-expect-error TS7016: Could not find a declaration file for module
+import InputBufferUnderrunError from 'thrift/lib/nodejs/lib/thrift/input_buffer_underrun_error';
+
+export class THTTPException extends Thrift.TApplicationException {
+  public readonly statusCode: unknown;
+
+  public readonly response: Response;
+
+  constructor(response: Response) {
+    super(
+      Thrift.TApplicationExceptionType.PROTOCOL_ERROR,
+      `Received a response with a bad HTTP status code: ${response.status}`,
+    );
+    this.statusCode = response.status;
+    this.response = response;
+  }
+}
+
+type TTransportType = typeof TBufferedTransport;
+
+interface ThriftHttpConnectionOptions {
+  url: string;
+  transport?: TTransportType;
+  protocol?: TProtocolConstructor;
+}
+
+// This type describes a shape of internals of Thrift client object.
+// It is not perfect good enough for our needs
+type ThriftClient = {
+  // Internal map of callbacks of running requests. Once request is completed (either successfully or not) -
+  // callback should be removed from it
+  _reqs: Record<number, (error: unknown, response?: unknown) => void>;
+} & {
+  // For each client's public method Foo there are two private ones: send_Foo and recv_Foo.
+  // We have to access recv_Foo ones to properly parse the response
+  [key: string]: (input: TProtocol, mtype: Thrift.MessageType, seqId: number) => void;
+};
+
+export default class ThriftHttpConnection extends EventEmitter {
+  private readonly url: string;
+
+  private readonly config: RequestInit;
+
+  // This field is used by Thrift internally, so name and type are important
+  private readonly transport: TTransportType;
+
+  // This field is used by Thrift internally, so name and type are important
+  private readonly protocol: TProtocolConstructor;
+
+  // thrift.createClient sets this field internally
+  public client?: ThriftClient;
+
+  constructor(options: ThriftHttpConnectionOptions, config: RequestInit = {}) {
+    super();
+    this.url = options.url;
+    this.config = config;
+    this.transport = options.transport ?? TBufferedTransport;
+    this.protocol = options.protocol ?? TBinaryProtocol;
+  }
+
+  public write(data: Buffer, seqId: number) {
+    const requestConfig: RequestInit = {
+      ...this.config,
+      method: 'POST',
+      headers: {
+        ...this.config.headers,
+        Connection: 'keep-alive',
+        'Content-Length': `${data.length}`,
+        'Content-Type': 'application/x-thrift',
+      },
+      body: data,
+    };
+
+    fetch(this.url, requestConfig)
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new THTTPException(response);
+        }
+
+        return response.buffer();
+      })
+      .then((buffer) => {
+        this.transport.receiver((transportWithData) => this.handleThriftResponse(transportWithData), seqId)(buffer);
+      })
+      .catch((error) => {
+        if (error instanceof FetchError) {
+          if (error.type === 'request-timeout') {
+            error = new Thrift.TApplicationException(
+              Thrift.TApplicationExceptionType.PROTOCOL_ERROR,
+              'Request timed out',
+            );
+          }
+        }
+
+        const defaultErrorHandler = (err: unknown) => {
+          this.emit('error', err);
+        };
+
+        if (this.client) {
+          const callback = this.client._reqs[seqId] ?? defaultErrorHandler;
+          delete this.client._reqs[seqId];
+          callback(error);
+        } else {
+          defaultErrorHandler(error);
+        }
+      });
+  }
+
+  private handleThriftResponse(transportWithData: TTransport) {
+    if (!this.client) {
+      throw new Thrift.TApplicationException(Thrift.TApplicationExceptionType.INTERNAL_ERROR, 'Client not available');
+    }
+
+    const Protocol = this.protocol;
+    const proto = new Protocol(transportWithData);
+    try {
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        const header = proto.readMessageBegin();
+        const dummySeqId = header.rseqid * -1;
+        const { client } = this;
+
+        client._reqs[dummySeqId] = (err, success) => {
+          transportWithData.commitPosition();
+          const clientCallback = client._reqs[header.rseqid];
+          delete client._reqs[header.rseqid];
+          if (clientCallback) {
+            process.nextTick(() => {
+              clientCallback(err, success);
+            });
+          }
+        };
+
+        if (client[`recv_${header.fname}`]) {
+          client[`recv_${header.fname}`](proto, header.mtype, dummySeqId);
+        } else {
+          delete client._reqs[dummySeqId];
+          throw new Thrift.TApplicationException(
+            Thrift.TApplicationExceptionType.WRONG_METHOD_NAME,
+            'Received a response to an unknown RPC function',
+          );
+        }
+      }
+    } catch (error) {
+      if (error instanceof InputBufferUnderrunError) {
+        transportWithData.rollbackPosition();
+      } else {
+        throw error;
+      }
+    }
+  }
+}
