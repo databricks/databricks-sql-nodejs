63f692b6c23240975d2b67078786435a8c39a541
Shivam Raj <shivam.raj@databricks.com>
2025-04-17 16:36:02 +0530

    minor changes

--- benchmarkNode.js
+++ benchmarkNode.js
@@ -17,7 +17,7 @@ if (!config.host || !config.path || !config.token) {
   console.error("ERROR: Please set DATABRICKS_SERVER_HOSTNAME, DATABRICKS_HTTP_PATH, and DATABRICKS_TOKEN environment variables.");
   process.exit(1);
 }
-const ITERATIONS = 10;
+const ITERATIONS = 7;
 // Folder that contains your SQL files
 const benchmarkingDir = path.join(__dirname, "benchmarking");
 
@@ -27,18 +27,18 @@ const modes = [
     modeName: "CF_on_LZ4_on",
     clientConfig: { host: config.host, path: config.path, token: config.token, useCloudFetch: true, useLZ4Compression: true }
   },
-  {
-    modeName: "CF_on_LZ4_off",
-    clientConfig: { host: config.host, path: config.path, token: config.token, useCloudFetch: true, useLZ4Compression: false }
-  },
+  // {
+  //   modeName: "CF_on_LZ4_off",
+  //   clientConfig: { host: config.host, path: config.path, token: config.token, useCloudFetch: true, useLZ4Compression: false }
+  // },
   {
     modeName: "CF_off_LZ4_on",
     clientConfig: { host: config.host, path: config.path, token: config.token, useCloudFetch: false, useLZ4Compression: true }
   },
-  {
-    modeName: "CF_off_LZ4_off",
-    clientConfig: { host: config.host, path: config.path, token: config.token, useCloudFetch: false, useLZ4Compression: false }
-  }
+  // {
+  //   modeName: "CF_off_LZ4_off",
+  //   clientConfig: { host: config.host, path: config.path, token: config.token, useCloudFetch: false, useLZ4Compression: false }
+  // }
 ];
 
 /**
@@ -65,18 +65,17 @@ async function runQueryForMode(queryName, queryText, modeName, clientConfig) {
     const session = await client.openSession();
     await session.executeStatement("SET use_cached_result = false");
 
-    await benchfoodClient.connect({
-      host: 'e2-benchfood.cloud.databricks.com',
-      path: '/sql/1.0/warehouses/6e681b20741e4674',
-      token: 'xx'
-    })
-    const benchfoodSession = await benchfoodClient.openSession();
+    // await benchfoodClient.connect({
+    //   host: 'e2-benchfood.cloud.databricks.com',
+    //   path: '/sql/1.0/warehouses/6e681b20741e4674',
+    //   token: 'xx'
+    // })
+    // const benchfoodSession = await benchfoodClient.openSession();
 
     const times = [];
     for (let i = 0; i < ITERATIONS; i += 1) {
       const t0 = performance.now();
       const operation = await session.executeStatement(queryText, { useCloudFetch: clientConfig.useCloudFetch, useLZ4Compression: clientConfig.useLZ4Compression });
-      let rowCount = 0;
       // let chunk;
       // const fetchSize = 2000000; // High maxRows value
       // let peakHeap = process.memoryUsage().heapUsed;
@@ -87,24 +86,24 @@ async function runQueryForMode(queryName, queryText, modeName, clientConfig) {
       //   const currentHeap = process.memoryUsage().heapUsed;
       //   if (currentHeap > peakHeap) peakHeap = currentHeap;
       // } while (await operation.hasMoreRows());
-    await operation.fetchAll();
+    const rows = await operation.fetchAll();
 
       await operation.close();
       const t1 = performance.now();
       times.push(t1 - t0);
-      result.rowCount = rowCount;
+      result.rowCount = rows.length;
 
       // benchfood
       // host: e2-benchfood.cloud.databricks.com
       // path: /sql/1.0/warehouses/6e681b20741e4674
       // token: xx
 
-      const insertQuery = `
-          INSERT INTO main.node_benchmarking_schema.benchmarking_results
-          VALUES (CURRENT_TIMESTAMP, modeName, '${queryName}', ${i + 1}, ${Math.round(t1-t0)});
-      `;
+      // const insertQuery = `
+      //     INSERT INTO main.node_benchmarking_schema.benchmarking_results
+      //     VALUES (CURRENT_TIMESTAMP, modeName, '${queryName}', ${i + 1}, ${Math.round(t1-t0)});
+      // `;
       console.log("modeName: ", modeName, "queryName: ", queryName, "iteration: ", i + 1, "time: ", Math.round(t1-t0));
-      await benchfoodSession.executeStatement(insertQuery);
+      // await benchfoodSession.executeStatement(insertQuery);
 
       // add a delay to avoid hitting the rate limit
       await new Promise(resolve => setTimeout(resolve, 1000)); // 1 second delay
@@ -115,16 +114,21 @@ async function runQueryForMode(queryName, queryText, modeName, clientConfig) {
     // const medianTime = times[Math.floor(times.length / 2)];
 
     // result.time_ms = Math.round(medianTime);
-    // const trimmedTimes = times.slice(1, -1);
-    const sum = times.reduce((a, b) => a + b, 0);
-    const averageTime = sum / times.length;
+    const trimmedTimes = times.slice(1, -1);
+    // const sum = times.reduce((a, b) => a + b, 0);
+    // const averageTime = sum / times.length;
+    const sum = trimmedTimes.reduce((a, b) => a + b, 0);
+    const averageTime = sum / trimmedTimes.length;
+
+    console.log("modeName: ", modeName, "queryName: ", queryName, "averageTime: ", Math.round(averageTime));
+
 
     result.time_ms = Math.round(averageTime);
     result.peakMemory_MB = (process.memoryUsage().heapUsed / (1024 * 1024)).toFixed(1);
 
     await session.close();
     await client.close();
-    await benchfoodSession.close();
+    // await benchfoodSession.close();
     await benchfoodClient.close();
     if (global.gc) { global.gc(); }
   } catch (err) {
