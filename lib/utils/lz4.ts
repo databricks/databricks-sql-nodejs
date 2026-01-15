6b5e8034d862ed466e0da98b561d6284deadae96
Tuukka Ikkala <tuukka.ikkala@reaktor.com>
2026-01-15 13:07:49 +0200

    Avoid calling require('lz4') if it's really not required
    This changes the utlity module to return a function instead of the module directly. This way, caller can control when the module is actually tried to be resolved.
    
    Signed-off-by: Tuukka Ikkala <tuukka.ikkala@reaktor.com>

--- lib/utils/lz4.ts
+++ lib/utils/lz4.ts
@@ -7,6 +7,7 @@ function tryLoadLZ4Module(): LZ4Module | undefined {
     return require('lz4'); // eslint-disable-line global-require
   } catch (err) {
     if (!(err instanceof Error) || !('code' in err)) {
+      // eslint-disable-next-line no-console
       console.warn('Unexpected error loading LZ4 module: Invalid error object', err);
       return undefined;
     }
@@ -16,14 +17,26 @@ function tryLoadLZ4Module(): LZ4Module | undefined {
     }
 
     if (err.code === 'ERR_DLOPEN_FAILED') {
+      // eslint-disable-next-line no-console
       console.warn('LZ4 native module failed to load: Architecture or version mismatch', err);
       return undefined;
     }
 
     // If it's not a known error, return undefined
+    // eslint-disable-next-line no-console
     console.warn('Unknown error loading LZ4 module: Unhandled error code', err);
     return undefined;
   }
 }
 
-export default tryLoadLZ4Module();
+// The null is already tried resolving that failed
+let resolvedModule: LZ4Module | null | undefined;
+
+function getResolvedModule() {
+  if (resolvedModule === undefined) {
+    resolvedModule = tryLoadLZ4Module() ?? null;
+  }
+  return resolvedModule === null ? undefined : resolvedModule;
+}
+
+export default getResolvedModule;
